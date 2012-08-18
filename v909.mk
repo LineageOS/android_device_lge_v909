ab66eab6308c1a4a12aca855f5c5fa904d5315b4
Janne Grunau <janne-android@jannau.net>
2012-08-18 11:50:47 +0200

    use bugmailer for Vol+,Vol- triggered reports
    Change-Id: Ic005df92fee6de6e11606e4315994939eef986a1

--- v909.mk
+++ v909.mk
@@ -92,6 +92,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
+# for bugmailer
+PRODUCT_PACKAGES += send_bug
+PRODUCT_COPY_FILES += \
+    system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
+    system/extras/bugmailer/send_bug:system/bin/send_bug
+
 # Inherit tablet dalvik settings
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 
